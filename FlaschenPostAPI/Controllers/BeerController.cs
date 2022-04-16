b5a66a2cf9209cd2ae1e81d205faacbe21e43742
Nico F <35654361+NicoSoftware@users.noreply.github.com>
2022-04-16 17:24:34 +0200

    Initial Commit

---
+++ FlaschenPostAPI/Controllers/BeerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FlaschenPostAPI.Repo;
+using FlaschenpostModels.Models;
+
+namespace FlaschenPostAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BeerController : Controller
+    {
+        [HttpGet]
+        [Route("TeuerstesBier")]
+        public List<Beer> GetTeuerstesBier()
+        {
+            FlaschenpostGetRepo Repo = new FlaschenpostGetRepo();
+            //return Repo.GetMostExpensiveBeer();
+            return Repo.GetAllBeer();
+        }
+
+        [HttpGet]
+        [Route("BilligstesBier")]
+        public List<Beer> GetBilligstesBier()
+        {
+            FlaschenpostGetRepo Repo = new FlaschenpostGetRepo();
+            return Repo.GetCheapestBeer();
+        }
+
+        //Auf Parameter zwecks Requesthandling verzichtet
+        [HttpGet]
+        [Route("BeerWithPrice17_99")]
+        public IEnumerable<Beer> GetBeersbyPrice()
+        {
+            FlaschenpostGetRepo Repo = new FlaschenpostGetRepo();
+            return Repo.GetBeersByPrice(17.99);
+        }
+
+        [HttpGet]
+        [Route("MostBottledBeer")]
+        public Article GetBeersWithMostBottles()
+        {
+            FlaschenpostGetRepo Repo = new FlaschenpostGetRepo();
+            return Repo.GetMostbootledBeer();
+        }
+    }
+}
---
+++ FlaschenPostAPI/Repo/FlaschenpostGetRepo.cs
@@ -0,0 +1,105 @@
+﻿
+using FlaschenpostModels.Interfaces.Repositories;
+using FlaschenPostAPI.Helper;
+using FlaschenpostModels.Models;
+
+#nullable disable
+
+namespace FlaschenPostAPI.Repo
+{
+    public class FlaschenpostGetRepo : IBierGetRepo
+    {
+        public List<Beer> GetCheapestBeer()
+        {
+            return null;
+        }
+        public List<Beer> GetAllBeer()
+        {
+            return BeerHelper.GetBeerData();
+        }
+
+        /// <summary>
+        /// Gibt die Angebote von Bier zurück, die dem übergebenen Preis entsprechen
+        /// -> Keine Userstory dazu, jedoch für mich ohne Erfahrung über Domain wesentlich sinnvoller
+        /// </summary>
+        public IEnumerable<Beer> GetBeersByPrice(double price)
+        {
+            List<Beer> ListofBeers = BeerHelper.GetBeerData();
+            List<Beer> RtnList = new List<Beer>();
+
+            foreach (var Beer in ListofBeers)
+            {
+                bool isExactPrice = false;
+                foreach (var Article in Beer.Angebote)
+                {
+                    if (Article.price == 17.99)
+                    {
+                        if (isExactPrice == true)
+                        {
+                            RtnList.Add(Beer);
+                        }
+                    }
+                }
+                return RtnList.Sort(Beer => Beer.Angebote.
+            }
+        }
+        /// <summary>
+        /// Gibt das erste Angebot zurück, welches die Meisten Flaschen besitzt
+        /// "Which one product comes in the most bottles?"
+        /// </summary>
+        public Article GetMostbootledBeer()
+        {
+            List<Beer> ListofBeers = BeerHelper.GetBeerData();
+            Article article = null;
+            foreach (Beer Beer in ListofBeers)
+            {
+                foreach (Article BeerArticle in Beer.Angebote)
+                {
+                    if (article == null)
+                    {
+                        article = BeerArticle;
+                    }
+                    else
+                    {
+                        double ThisArcticleBottles = BeerHelper.GetAmountBottles(article.pricePerUnitText);
+                        double NextArticleBottles = BeerHelper.GetAmountBottles(BeerArticle.pricePerUnitText);
+
+                        //Der erste Eintrag mit der höchsten Anzahl verändert sich so nicht mehr
+                        if (ThisArcticleBottles < NextArticleBottles) 
+                        {
+                            article = BeerArticle;
+                        }
+                    }
+                }
+            }
+            return article;
+        }
+
+        /// <summary>
+        /// Gibt das teuerste Bier im Sortiment zurück
+        /// </summary>
+        public List<Beer> GetMostExpensiveBeer()
+        {
+            List<Beer> ListofBeers = BeerHelper.GetBeerData();
+            List<Beer> MostExpensiveBeer = new List<Beer>();
+            MostExpensiveBeer.Add(ListofBeers[0]);
+
+            foreach (var Beer in ListofBeers)
+            {
+                foreach (var Article in Beer.Angebote)
+                {
+
+                }
+            }
+            return MostExpensiveBeer;
+        }
+
+        /// <summary>
+        /// Zwinkersmiley
+        /// </summary>
+        public Beer GetBestBeer()
+        {
+            return BeerHelper.GetBeerData().Where(Beer => Beer.Id == 70).FirstOrDefault();
+        }
+    }
+}
